793a1b5ae11123d68a69477c70066453b111e006
YahYahDev <159183828+YahYahDev@users.noreply.github.com>
2024-05-20 14:46:58 -0400

    Add files via upload

---
+++ Yellow-Jacket-Theme/Yellow-Jacket-Theme/install.sh
@@ -0,0 +1,48 @@
+# This script has only been tested for arch linux with awesomewm
+# make adjustments as needed
+
+
+
+# Install ::TERMINAL::
+# allacritty, picom
+# picom is needed as a compositor for fancy stuff
+    sudo pacman -S alacritty picom
+
+    # Copy alacritty config files
+    rm /home/$USER/.config/alacrity.toml
+    cp alacritty/alacritty.toml /home/$USER/.config/alacrity/
+    
+# Install ::SHELL::
+# zsh, oh-my-zsh, zsh-syntax-highlighting, zsh-autosuggestions, ect
+    sudo pacman -S zsh zsh-completions
+    # Installs zsh and zsh-completions from pacman
+    
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    # Link for above https://github.com/ohmyzsh/ohmyzsh
+    
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    # Link for above https://github.com/zsh-users/zsh-syntax-highlighting
+    
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    # Link for above https://github.com/zsh-users/zsh-autosuggestions
+
+    # Copy zsh configs to apropreate spots
+    
+    
+    
+    
+# Install ::TEXT EDITORS::
+# micro, xclip, textadept
+# xclip is needed to use clip board
+    sudo pacman -S micro xclip
+    
+    mkdir /home/$USER/.config/micro/colorchemes # Incase dir does not exist
+    
+    cp micro/colorschemes/Yellow-Jacket /home/$USER/.config/micro/colorschemes/
+    
+    rm /home/$USER/.config/micro/bindings.json
+    rm /home/$USER/.config/micro/settings.json
+    
+    cp micro/bindings.json /home/$USER/.config/micro/
+    cp micro/settings.json /home/$USER/.config/micro/
+
